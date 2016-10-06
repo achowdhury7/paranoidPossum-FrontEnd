4a8a6d683a69158a837f1d6534591e132a15b816
Abhijit <abhijit.chowdhury10192@gmail.com>
2016-10-06 19:20:01 +0530

    search widget added

--- public/scripts/controllers/HeaderCtrl.js
+++ public/scripts/controllers/HeaderCtrl.js
@@ -1,9 +1,11 @@
 angular
 	.module('paranoidPossumApp')
-	.controller('HeaderCtrl', ['$scope', function($scope) {
+	.controller('HeaderCtrl', ['$scope', 'postFactory', function($scope, postFactory) {
 		$scope.$on('header-update', function(event, header) {
 			$scope.title = header.title;
 			$scope.subtitle = header.subtitle;
 			$scope.page = header.page;
 		});
+
+		$scope.posts = postFactory.posts;
 	}]);
